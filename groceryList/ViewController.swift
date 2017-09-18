0e7a0fd7dd74e4692733d20678140479dde56f65
apcs2 <31315416+PKPowerHouse@users.noreply.github.com>
2017-09-18 09:46:06 -0500

    aaa
    jkhkh

--- groceryList/ViewController.swift
+++ groceryList/ViewController.swift
@@ -5,12 +5,12 @@
 //  Created by apcs2 on 9/15/17.
 //  Copyright © 2017 apcs2. All rights reserved.
 //
-
+//
 import UIKit
 
 class ViewController: UIViewController
 {
-
+    var test = 1
     override func viewDidLoad()
     {
         super.viewDidLoad()
