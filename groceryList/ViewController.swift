deb2f9f30e8d96e7029a2d45ede8c32ef246eec6
apcs2 <apcs2@p-162mba-05.d214.org>
2017-09-18 09:21:18 -0500

    no message

--- groceryList/ViewController.swift
+++ groceryList/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+import CoreData
+var array: [NSManagedObject] = []
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    @IBOutlet weak var tableList: UITableView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Entity")
+        do
+        {
+            array = try fetchFunction(fetchRequest as! NSFetchRequest<NSFetchRequestResult>) as! [NSManagedObject]
+        }
+        catch
+        {
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func fetchFunction(_ request: NSFetchRequest<NSFetchRequestResult>) throws -> [Any]
+    {
+        var array2 : [Any]! = []
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let manageContext = appDelegate?.persistentContainer.viewContext
+        do
+        {
+            array2 = try manageContext?.fetch(request)
+        }
+        catch
+        {
+            print("varsity game Friday 6:30")
+        }
+        return array2
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row].value(forKeyPath: "text") as? String
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return array.count
+    }
+    func save(text: String, decimal: Float)
+    {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else
+        {
+            return
+        }
+        let manageContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Entity", in: manageContext)!
+        let newObject = NSManagedObject(entity: entity, insertInto: manageContext)
+        newObject.setValue(text, forKey: "text")
+        newObject.setValue(decimal, forKey: "decimal")
+        do
+        {
+            try manageContext.save()
+        }
+        catch
+        {
+            print("STRING")
+        }
+    }
 }
 
