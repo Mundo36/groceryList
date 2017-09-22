6a55b55b64fa827e2af092ab28ffdefec98e15c1
apcs2 <31315416+PKPowerHouse@users.noreply.github.com>
2017-09-22 09:40:47 -0500

    Adding more stuff

---
+++ groceryList/Catagories.swift
@@ -0,0 +1,20 @@
+//
+//  Catagories.swift
+//  groceryList
+//
+//  Created by apcs2 on 9/22/17.
+//  Copyright © 2017 apcs2. All rights reserved.
+//
+
+import Foundation
+
+class groceryCatagory
+{
+    var protein = []
+    var fish = []
+    var fruit = []
+    var veggies = []
+    var drinks = []
+    var grains = []
+    var dairy = []
+}
